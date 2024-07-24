2e8601b2ff1b49d381e0dc26ef98a85f1046e680
libin <libin@zlongame.com>
2024-07-24 18:43:52 +0800

    <test>

---
+++ auto_fight/tree/constant.py
@@ -0,0 +1,25 @@
+HERO = {
+    "id": 1,
+    "name": "Jinx",
+    "hp": 100,
+    "skill_A": {"name": "火球术", "range": 4, "damage": 20, "max_uses": 3, "attack_type": "single"},
+    "skill_B": {"name": "闪电⚡️", "range": 100, "damage": 10, "max_uses": 3, "attack_type": "group"},
+    "normal_attack_damage": 5,
+    "normal_attack_range": 4,
+    "position": (5, 5),
+    "aggro": {"A": 10, "B": 0}
+}
+
+ENEMY_A = {
+    "id": 10,
+    "name": "A",
+    "hp": 100,
+    "position": (3, 3)
+}
+
+ENEMY_B = {
+    "id": 11,
+    "name": "B",
+    "hp": 100,
+    "position": (6, 6),
+}
