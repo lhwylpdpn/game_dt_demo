af42ca12a62ccabc6f002bf7bb7b7ce0a7608e03
zhaohu <254560330@qq.com>
2024-07-29 11:11:56 +0800

    <feature> add monstor

---
+++ V1/test_monster_data.py
@@ -0,0 +1,63 @@
+origin_monster_data = {
+    "sn": 1,
+    "MonsterId": 1173,
+    "MonsterClass": 1,
+    "Name": "骷髅怪",
+    "Rank": 1,
+    "Clazz": 2,
+    "Features": 0,
+    "Level": 1,
+    "natures": [
+
+    ],
+    "Hp": 1000,
+    "Atk": 10,
+    "Def": 0,
+    "AtkDistance": [
+        0,
+        1
+    ],
+    "Velocity": 150,
+    "HateRange": 5,
+    "Counterattack": 0,
+    "ForwardShake": 0.5,
+    "HitForwardShake": 0.1,
+    "AttributetypeIcon": "texture/icon/Hero_Attributetype/1",
+    "Weapons": "Art_3d/Weapon/1173_Sword/Prefab_1173_Sword",
+    "WeaponsId": "10001",
+    "ApproachEffect": 0,
+    "Duration": 0,
+    "Skid1": 30,
+    "Skid2": 0,
+    "Skid3": 0,
+    "exp": 0,
+    "RoundAction": 6,
+    "JumpHeight": [
+        6,
+        6
+    ],
+    "SpecialItems": 4,
+    "position": [
+        5,
+        5,
+        7
+    ],
+    "skills": [
+        {
+            "sn": 170,
+            "SkillId": 30,
+            "SkillLev": 1,
+            "SkillSpine": "",
+            "effects": [
+                {
+                    "id": 1,
+                    "key": "",
+                    "param": [
+                        "0"
+                    ],
+                    "tag": ""
+                }
+            ]
+        }
+    ]
+}
