bb7800508dae8831649cc312e07f9725ab3bdae2
lhwylp <58254451@qq.com>
2024-08-02 13:43:53 +0800

    1

--- V1/schedule.py
+++ V1/schedule.py
@@ -7,14 +7,9 @@ from test_hero_data import origin_hero_data  # 后续通过api获取前端传递
 from test_map_data import origin_map_data  # 后续通过api获取前端传递的数据
 from test_monster_data import origin_monster_data  # 后续通过api获取前端传递的数据
 import copy
-import jsondiff
 import math
-import pandas as pd
-from deepdiff import DeepDiff
-import re
-import dictdiffer
-
-
+from utils.tools import Deepdiff_modify
+from utils.tools import performance
 # step0 调度接到外部的开始请求，传入初始地图，传入初始角色，传入计算信息
 # step1 调度开始游戏,调用棋盘初始化游戏
 # step2 调度开始自增计时器
@@ -25,35 +20,6 @@ import dictdiffer
 # step4.6 获得行动后的状态，计算状态差异，组织数据存储
 # step5 检查游戏是否结束
 # step6 如果游戏结束，产生内容序列
-
-
-class performance:
-    def __init__(self):
-        self.start_time = time.time()
-        self.result = {}
-        self.tag_time={}
-        self.event_count={}
-    def event_start(self, event_name):
-        self.tag_time[event_name]=time.time()
-    def event_end(self, event_name):
-        use_time=time.time()-self.tag_time[event_name]
-        self.result[event_name]=use_time if self.result.get(event_name) is None else self.result[event_name]+use_time
-        self.event_count[event_name]=self.event_count[event_name]+1 if self.event_count.get(event_name) is not None else 1
-        self.tag_time[event_name]=None
-
-    def end(self):
-        self.result['total_time']=time.time()-self.start_time
-        self.event_count['total_time']=1
-
-
-    def static(self):
-        #计算各类事件的平均时间转成df
-        df=pd.DataFrame(self.result.items(),columns=['event','time'])
-        #合并每个事件的次数
-        df['count']=df['event'].apply(lambda x:self.event_count[x])
-        df['avg_time']=df['time']/df['count']
-        print(df)
-
 
 class schedule:
 
@@ -107,12 +73,12 @@ class schedule:
                 if hero.__class__.__name__.lower() == 'hero':
                     self.performance.event_start('schedule_choose_action')
                     actions = self.agent_1.choice_hero_act(hero, state)
-                    print('tick',self.tick,'调度获得的行动list: 英雄', hero.HeroID, actions)
+                    #print('tick',self.tick,'调度获得的行动list: 英雄', hero.HeroID, actions)
                     self.performance.event_end('schedule_choose_action')
                 if hero.__class__.__name__.lower() == 'monster':
                     self.performance.event_start('schedule_choose_action')
                     actions = self.agent_2.choice_monster_act(hero, state)
-                    print('tick',self.tick,'调度获得的行动list: 怪兽', hero.MonsterId, actions)
+                    #print('tick',self.tick,'调度获得的行动list: 怪兽', hero.MonsterId, actions)
                     self.performance.event_start('schedule_choose_action')
 
                 for action in actions:
@@ -183,52 +149,31 @@ class schedule:
 
 
         #打印测试
-        for key in self.record_update_dict.keys():
-            print('第',key,'tick 行动')
-            print('行动',self.record_update_dict[key]['action'])
-            print('状态',self.record_update_dict[key]['state'])
+        # for key in self.record_update_dict.keys():
+        #     print('第',key,'tick 行动')
+        #     print('行动',self.record_update_dict[key]['action'])
+        #     print('状态',self.record_update_dict[key]['state'])
 
-
+        print(self.record_update_dict)
         return self.record_update_dict
-
-
-def Deepdiff_modify(before,after):
-    diff_types = [
-        "add",
-        "remove",
-        "change",
-    ]
-
-    res=list(dictdiffer.diff(before, after))
-    print('Deepdiff_modify',res)
-
-    for i in res:
-        if i[0]=='change':
-            path=i[1]
-            tmp=after
-            for _ in path:
-                tmp=tmp[_]
-            #print(tmp)
-    #print('对比结果',jsondiff.diff(before,after))
-    # print('对比结果2',list(dictdiffer.diff(before, after)))
-    return list(dictdiffer.diff(before, after))
-
 
 
 if __name__ == '__main__':
     map = BuildPatrol.build_map(origin_map_data)  # map
     heros = BuildPatrol.build_heros(origin_hero_data)  # heros
     monster = BuildPatrol.build_monster(origin_monster_data)
-
-    heros[0].set_x(1)
-    heros[0].set_y(1)
-    heros[0].set_z(1)
-    heros[0].set_Atk(1)
-    heros[0].set_max_step(300)
-    heros[1].set_max_step(300)
-    monster[0].set_x(100)
-    monster[0].set_Atk(10000)
-    monster[0].set_max_step(10000)
+    #
+    # heros[0].set_x(1)
+    # heros[0].set_y(1)
+    # heros[0].set_z(1)
+    # heros[0].set_Atk(1)
+    # heros[0].set_max_step(300)
+    # heros[1].set_max_step(300)
+    # heros[1].set_dogBase(10000)
+    # heros[0].set_dogBase(10000)
+    # monster[0].set_x(100)
+    # monster[0].set_Atk(10000)
+    # monster[0].set_max_step(10000)
 
     #heros[0].set_max_step(3)
 
---
+++ V1/utils/tools.py
@@ -0,0 +1,86 @@
+import dictdiffer
+import time
+import pandas as pd
+
+
+
+class performance:
+    def __init__(self):
+        self.start_time = time.time()
+        self.result = {}
+        self.tag_time={}
+        self.event_count={}
+    def event_start(self, event_name):
+        self.tag_time[event_name]=time.time()
+    def event_end(self, event_name):
+        use_time=time.time()-self.tag_time[event_name]
+        self.result[event_name]=use_time if self.result.get(event_name) is None else self.result[event_name]+use_time
+        self.event_count[event_name]=self.event_count[event_name]+1 if self.event_count.get(event_name) is not None else 1
+        self.tag_time[event_name]=None
+
+    def end(self):
+        self.result['total_time']=time.time()-self.start_time
+        self.event_count['total_time']=1
+
+
+    def static(self):
+        #计算各类事件的平均时间转成df
+        df=pd.DataFrame(self.result.items(),columns=['event','time'])
+        #合并每个事件的次数
+        df['count']=df['event'].apply(lambda x:self.event_count[x])
+        df['avg_time']=df['time']/df['count']
+        print(df)
+
+def Deepdiff_modify(before,after):
+
+    res=list(dictdiffer.diff(before, after))
+    #print('Deepdiff_modify_before',res)
+    return_res={}
+    for i in res:
+        if i[0]=='change':
+            path=i[1]
+            res={}
+            tmp={}
+            value_tmp=after
+            for _ in path:
+                value_tmp=value_tmp[_]
+            res = {path[-1]: value_tmp}
+            path=path[::-1][1:]
+            for _ in path:
+                tmp={}
+                tmp[_]=res.copy()
+                res=tmp
+
+        if i[0]=='add':
+            path=i[1]
+            res={}
+            tmp={}
+            value_tmp=after
+            for _ in path:
+                value_tmp=value_tmp[_]
+            res = {path[-1]: value_tmp}
+            path=path[::-1][1:]
+            for _ in path:
+                tmp={}
+                tmp[_]=res.copy()
+                res=tmp
+
+        merge_dicts(return_res,res)
+
+
+
+    return return_res
+
+
+def merge_dicts(dict_a, dict_b):
+    for key in dict_b:
+        if key in dict_a:
+            # 如果值都是字典，则递归合并
+            if isinstance(dict_a[key], dict) and isinstance(dict_b[key], dict):
+                merge_dicts(dict_a[key], dict_b[key])
+            else:
+                # 否则，直接赋值
+                dict_a[key] = dict_b[key]
+        else:
+            # 如果在 A 中不存在，直接添加
+            dict_a[key] = dict_b[key]
