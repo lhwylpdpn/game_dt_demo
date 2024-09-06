f6ae9a334ea37dd97e4dfde39e6122bdd4b78705
libin <libin@zlongame.com>
2024-09-06 10:58:15 +0800

    <治疗函数>

---
+++ V1/utils/heal.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Author  : Bin
+# @Time    : 2024/9/5 17:18
+
+from utils.config import demo_skill
+
+
+def heal(caster, target, skill):
+    heal_amount = 0
+
+    # 释放者属性
+    caster_magical_atk = caster.MagicalAtk
+
+    # 受动者属性
+    target_hp = target.Hp
+
+    if skill.SkillId == demo_skill['治疗大型治疗']:
+        heal_amount = float(caster_magical_atk * 2.4)  # 240%
+
+    if skill.SkillId == demo_skill['治疗持续治疗']:
+        heal_amount = float(target_hp * 0.083)  # 8.3%
+
+    if skill.SkillId == demo_skill['治疗强力治愈']:
+        heal_amount = float(caster_magical_atk * 1.5)  # 150%
+
+    return [{'heal': heal_amount, "pre_heal": heal}]
