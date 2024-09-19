f8f56011409569efe5744792ccce0ceadefd820a
lhwylp <58254451@qq.com>
2024-09-19 15:15:46 +0800

    增加单个英雄技能测试功能

--- V1/unit_test.py
+++ V1/unit_test.py
@@ -8,14 +8,19 @@ import schedule
 from V1.test.demo_show import game
 import random
 import copy
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+# 显示每一列的全部内容
+pd.set_option('display.max_colwidth', 1000)
+from V1.utils.config import demo_skill
 
 class test_process:
     def __init__(self):
-        # map = BuildPatrol.build_map(origin_map_data)  # map
-        # heros = BuildPatrol.build_heros(origin_hero_data)  # heros
-        # monster = BuildPatrol.build_monster(origin_monster_data)
-        # self.state = {"map": map, "hero": heros, "monster": monster}
-        self.state = BuildPatrol("data.json").load_data() 
+
+        self.state = BuildPatrol("data.json").load_data()
+        print('state',self.state)
 
     def data_init(self):
         p_all = self.state['map'].view_from_y_dict().keys()
@@ -111,17 +116,18 @@ class test_process:
             self.state['monster'][i].set_z(p[2])
 
 
+
     #创建固定参数
     def pygame_init(self):
         self.game_obj=game(self.state)
         self.game_obj.game_init()
 
-    def run(self):
+    def run(self,out_file_name='for_qiangye.json'):
 
         sch = schedule.schedule(self.state)
         sch.start()
         res=sch.run()
-        self.return_data = sch.send_update()
+        self.return_data = sch.send_update(out_file_name)
         sch.performance.static()
         return res
 
@@ -136,21 +142,72 @@ class test_process:
             print('monster',self.state['monster'][i].dict()['HeroID'],'HP',self.state['monster'][i].dict()['Hp'])
 
 
-if __name__ == '__main__':
-    res=[]
-    #显示pandas的所有行、列
-    import pandas as pd
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_rows', None)
-    #显示每一列的全部内容
-    pd.set_option('display.max_colwidth', 1000)
-    for i in range(1):
-        obj_=test_process()
-        #obj_.data_init()
+    def genarate_test_data_for_single_hero(self,basecalssid,skill_id):
+
+        #删除掉其他的英雄
+        update_hero=[]
+        update_monster = []
+        for i in range(len(self.state['hero'])):
+            if self.state['hero'][i].dict()['BaseClassID']==int(basecalssid):
+                update_hero.append(self.state['hero'][i])
+        #删除掉其他敌人,只留下精英怪
+        for i in range(len(self.state['monster'])):
+            if self.state['monster'][i].dict()['Quality']==int(2):
+                update_monster.append(self.state['monster'][i])
+
+        self.state['hero']=update_hero
+        self.state['monster']=[update_monster[0]]#只要一个怪物即可
+        p_all = self.state['map'].view_from_y_dict().keys()
+        p_all = list(p_all)
+        for p in self.state['map'].view_from_y_dict().keys():
+            if self.state['map'].view_from_y_dict()[p]['Block'] != 1:
+                p_all.remove(p)
+        for i in range(len(self.state['hero'])):
+            if self.state['hero'][i].dict()['BaseClassID']==int(basecalssid):
+                hero_random_RoundAction = random.randint(5,5)
+                hero_random_JumpHeight = [random.randint(15,15)]
+                hero_random_DogBase = random.randint(100,100)
+                hero_random_HP = random.randint(100000,100000)
+                hero_random_Atk = random.randint(200,200)
+                self.state['hero'][i].set_RoundAction(hero_random_RoundAction)
+                self.state['hero'][i].set_JumpHeight(hero_random_JumpHeight)
+                self.state['hero'][i].set_DogBase(hero_random_DogBase)
+                self.state['hero'][i].set_Hp(hero_random_HP)
+                self.state['hero'][i].set_Atk(hero_random_Atk)
+                self.state['hero'][i].set_AvailableSkills([skill_id])
+
+        self.state['monster'][0].set_x(self.state['hero'][0].dict()['position'][0]+1)
+        self.state['monster'][0].set_y(self.state['hero'][0].dict()['position'][1])
+        self.state['monster'][0].set_z(self.state['hero'][0].dict()['position'][2])
+        self.state['monster'][0].set_Atk(200)
+
+
+
+
+def single_skill_test_main(basecalssid,skill_id,pygame_init=True):
+    obj_=test_process()
+
+
+    obj_.genarate_test_data_for_single_hero(basecalssid=basecalssid,skill_id=skill_id)
+    if pygame_init:
         obj_.pygame_init()
-        res.append(obj_.run())
-        #obj_.over_state()
-        time.sleep(2)
+    obj_.run(out_file_name='for_qiangye_skill_'+str(skill_id)+'.json')
+    if pygame_init:
         obj_.game_run()
-    #print(res)
-    time.sleep(100)
+
+
+if __name__ == '__main__':
+    skill_list=[77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103]
+    #暂时有bug的技能
+    skill_list.remove(78)
+    skill_list.remove(97)
+    skill_list.remove(98)
+    skill_list.remove(99)
+    skill_list.remove(100)
+    skill_list.remove(101)
+    skill_list.remove(102)
+    skill_list.remove(103)
+
+
+    for skill_id in skill_list:
+        single_skill_test_main(basecalssid=1,skill_id=skill_id,pygame_init=False)
