022ff0f2bb27bacf7c8bbddf7519acdcaf670559
libin <34048165+qazxcvio@users.noreply.github.com>
2024-11-15 10:14:13 +0800

    <fix>

--- V1/strategy/action.py
+++ V1/strategy/action.py
@@ -115,9 +115,8 @@ class Action(object):
                 for _ in atk_res:
                     if atk_res[_].get("back_attck"):
                         atk_back = atk_res[_]["back_attck"]
-
-                        atk_back["id"] = atk_back["damage"][0]["effects"][0]["role_id"]
-                        atk_back["class"] = atk_back["damage"][0]["effects"][0]["role"]
+                        atk_back["id"] = atk_back["id"]
+                        atk_back["class"] = atk_back["class"]
                         atk_back["damage"] = self.calc_back_damage(atk_back)
                         back_res.append(atk_back)
 
