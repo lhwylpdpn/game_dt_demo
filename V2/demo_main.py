9811aaff58645b16306463221ccde1a8de0da0ad
libin <34048165+qazxcvio@users.noreply.github.com>
2025-01-13 17:33:53 +0800

    test

--- V2/demo_main.py
+++ V2/demo_main.py
@@ -3,6 +3,9 @@
 from twisted.internet.protocol import Factory
 from twisted.internet import protocol, reactor
 from twisted.protocols.basic import LineReceiver
+
+from msgs import card_game_pb2
+
 # from bin_test import card_game_pb2
 
 MAX_PLAYER = 2 # 房价最大人数
@@ -49,27 +52,27 @@ class CardGameProtocol(LineReceiver):
     
     def hand_Data(self, data):
         self.sendLine(f"Server Reciveced msg {data}".encode())
-        # try:
-        #     if len(data) < 4:
-        #         print("Incomplete data: waiting for length prefix")
-        #         return
+        try:
+            if len(data) < 4:
+                print("Incomplete data: waiting for length prefix")
+                return
 
-        #     length_prefix = int.from_bytes(data[:4], byteorder="big")
-        #     payload = data[4:4 + length_prefix]
+            length_prefix = int.from_bytes(data[:4], byteorder="big")
+            payload = data[4:4 + length_prefix]
 
-        #     request = card_game_pb2.ReadyGameRequest()
-        #     request.ParseFromString(payload)
-        #     print(f"Received Request: client_id={request.client_id}, game_id={request.game_id}")
+            request = card_game_pb2.ReadyGameRequest()
+            request.ParseFromString(payload)
+            print(f"Received Request: client_id={request.client_id}, game_id={request.game_id}")
 
-        #     response = card_game_pb2.ReadyGameResponse()
-        #     response.success = True
-        #     response.message = f"Player {request.client_id} is ready for game {request.game_id}"
+            response = card_game_pb2.ReadyGameResponse()
+            response.success = True
+            response.message = f"Player {request.client_id} is ready for game {request.game_id}"
 
-        #     serialized_response = response.SerializeToString()
-        #     length_prefix = len(serialized_response).to_bytes(4, byteorder="big")
-        #     self.transport.write(length_prefix + serialized_response)
-        # except Exception as e:
-        #     print(f"Error processing request: {e}")
+            serialized_response = response.SerializeToString()
+            length_prefix = len(serialized_response).to_bytes(4, byteorder="big")
+            self.transport.write(length_prefix + serialized_response)
+        except Exception as e:
+            print(f"Error processing request: {e}")
 
     
 class CardGameFactory(protocol.Factory):
